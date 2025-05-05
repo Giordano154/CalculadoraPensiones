3621fece045b9396306c141d700c18d0d1504de3
Michael <micgaelma@gmail.com>
2025-05-05 12:47:30 -0500

    Validacion de correo por Michael 05-05-25

--- js/validation.js
+++ js/validation.js
@@ -1,6 +1,24 @@
 function validarCorreo(correo) {
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(correo);
+  if (!email.includes("@")) return false;
+
+  const partes = email.split("@");
+  if (partes.length !== 2) return false;
+
+  const usuario = partes[0];
+  const dominio = partes[1];
+
+  if (usuario.length === 0 || dominio.length === 0) return false;
+  if (!dominio.includes(".")) return false;
+  if (email.includes(" ")) return false;
+  if (
+    email.startsWith(".") ||
+    email.endsWith(".") ||
+    email.startsWith("@") ||
+    email.endsWith("@")
+  )
+    return false;
+
+  return true;
 }
 
 const correo1 = "ejemplo@dominio.com";
